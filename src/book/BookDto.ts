34b7116cc11e5f232a69fc959a76f86179c333b2
bondarenko.vs@gmail.com <4MnT75G8>
2020-09-13 22:50:11 +0300

    Refactored

--- src/book/BookDto.ts
+++ src/book/BookDto.ts
@@ -1,11 +1,13 @@
 export class BookDto {
   name?: string;
+  description?: string;
   genreId?: number;
   authorsIds?: number[];
 
   constructor(data?: BookDto) {
     if (data) {
       this.name = data.name;
+      this.description = data.description;
       this.genreId = data.genreId;
       this.authorsIds = data.authorsIds && data.authorsIds.length > 0 ? data.authorsIds : [];
     }
